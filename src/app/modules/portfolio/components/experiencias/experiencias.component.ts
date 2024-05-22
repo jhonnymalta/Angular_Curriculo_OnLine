ca36ed82d3f370e9b0d1c23dc14207fac0b05b06
jhonnymalta <jhonatamalta@hotmail.com>
2024-05-22 19:58:05 -0300

    create experience section

--- src/app/modules/portfolio/components/experiencias/experiencias.component.ts
+++ src/app/modules/portfolio/components/experiencias/experiencias.component.ts
@@ -22,5 +22,17 @@ export class ExperienciasComponent {
       tarefas até a entrega ao cliente. Prezando pelo constante contato com o usuário,
       seguindo os ritos do SCRUM, afim de garantir uma melhor qualidade de entrega.`,
     },
+    {
+      summary:{
+        strong: 'Desenvolvedor DOTNET pleno',
+        p: 'StandBy Consultoria e Sistema LTDA | junho de 2023 até maio de 2024 - 11 meses',
+      },
+      text: `Desenvolvi novas funcionalidades em uma arquitetura moderna, criei
+      minuciosamente controllers, services, repositories e validations. Isso garantiu uma
+      separação eficiente de responsabilidades, abrindo caminho para manutenção
+      simplificada e escalabilidade. Participando desde o processo de entendimento das
+      tarefas até a entrega ao cliente. Prezando pelo constante contato com o usuário,
+      seguindo os ritos do SCRUM, afim de garantir uma melhor qualidade de entrega.`,
+    },
   ])
 }
